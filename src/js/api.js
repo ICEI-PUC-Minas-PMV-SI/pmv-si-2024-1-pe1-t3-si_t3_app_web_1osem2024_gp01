35063e55b99cad713e2e20686cfb7e6453e8c7a7
carlosilveira98 <silveira.caam@gmail.com>
2024-05-31 20:48:51 -0300

    alteracoes

--- src/js/api.js
+++ src/js/api.js
@@ -1,36 +1,34 @@
-function createTableFromJson(jsonData){
+function createTableFromJson(jsonData) {
+    const tableHeader = document.getElementById("table-header");
+    const tableBody = document.getElementById("table-body");
+    const headers = Object.keys(jsonData[0]);
 
-    const tableHeader = document.getElementById ("table-header") 
-    
-    const tableBody = document.getElementById ("table-body") 
-    
-    const headers = Object.keys (jsonData[0]) 
-    
-    headers.forEach (header =>{
-        const th = document.createElement ("th")
-        th.innerText = header
-        tableHeader.appendChild(th)
-    })
-    
-    jsonData.forEach (item =>{
-        const tr = document.createElement ("tr")
-        headers.forEach(header =>{
-            const td = document.createElement ("td")
-            td.innerText = item[header]
-            tr.appendChild(td)
-        }
-    
-    )
-    tableBody.appendChild(tr)
+    // Limpa a tabela antes de adicionar os novos dados
+    tableHeader.innerHTML = "";
+    tableBody.innerHTML = "";
 
-    })
+    headers.forEach(header => {
+        const th = document.createElement("th");
+        th.innerText = header;
+        tableHeader.appendChild(th);
+    });
+
+    jsonData.forEach(item => {
+        const tr = document.createElement("tr");
+        headers.forEach(header => {
+            const td = document.createElement("td");
+            td.innerText = item[header];
+            tr.appendChild(td);
+        });
+        tableBody.appendChild(tr);
+    });
 }
 
 const botaoTeste = document.getElementById("teste");
 
 // Carrega o JSON usando fetch
 fetch("apifake.json")
-    .then(Response => Response.json())
+    .then(response => response.json())
     .then(data => {
         // Atribui a função anônima ao evento 'onclick' do botão
         botaoTeste.onclick = () => {
